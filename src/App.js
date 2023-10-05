03e4fb758fd9b787e3d9b58270f2bd02c085cc9b
ricardozx24 <142248471+ricardozx24@users.noreply.github.com>
2023-10-05 09:28:08 -0500

    first commit forms project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SignUpForm from "./SignUpForm";
 
 function App() {
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comments: "",
+    isFriendly: true,
+    employment: "",
+    favColor: "",
+  });
+
+  console.log(formData.employment);
+
+  function handleChange(event) {
+    setFormData((prevFormData) => {
+      const { name, value, type, checked } = event.target;
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    // submitToApi(formData)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+        value={formData.firstName}
+      />
+
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+        value={formData.lastName}
+      />
+
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={handleChange}
+        name="email"
+        value={formData.email}
+      />
+      <textarea
+        value={formData.comments}
+        placeholder="Comments"
+        onChange={handleChange}
+        name="comments"
+      />
+
+      <input
+        type="checkbox"
+        id="isFriendly"
+        checked={formData.isFriendly}
+        onChange={handleChange}
+        name="isFriendly"
+      />
+      <label htmlFor="isFriendly">Are you friendly?</label>
+      <br />
+
+      <fieldset>
+        <legend>Current employment status</legend>
+
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          checked={formData.employment === "unemployed"}
+          onChange={handleChange}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+        <br />
+
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          checked={formData.employment === "part-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="part-time">Part-time</label>
+        <br />
+
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          checked={formData.employment === "full-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="full-time">Full-time</label>
+        <br />
+      </fieldset>
+      <label htmlFor="favColor">What is your favorite color?</label>
+      <br />
+
+      <select
+        id="favColor"
+        value={formData.favColor}
+        onChange={handleChange}
+        name="favColor">
+        <option value="">Choose Color</option>
+        <option value="red">Red</option>
+        <option value="orange">Orange</option>
+        <option value="yellow">Yellow</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+        <option value="indigo">Indigo</option>
+        <option value="violet">Violet</option>
+      </select>
+      <button>Submit Form</button>
+
+      <SignUpForm />
+    </form>
   );
 }
 
